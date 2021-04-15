c618aed845346b94dd68b16084073a35dfbf15fe
Anup Bangalore <anup.bangalore@gmail.com>
2021-04-15 21:39:17 +0200

    clean code. Seperate read and write.

--- OrderSplitter/Program.cs
+++ OrderSplitter/Program.cs
@@ -1,15 +1,50 @@
-﻿using System;
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using Microsoft.VisualBasic.FileIO;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
+using System.Linq;
 
 namespace OrderSplitter
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            using (var reader = new StreamReader("D:\\.Net\\OrderSplitter\\OrderSplitter\\orders.csv"))
+            Orders ords = new Orders();
 
-            Console.WriteLine("Hello World!");
+            var config = new CsvConfiguration(CultureInfo.InvariantCulture) { Delimiter = ";" };
+            using (var reader = new StreamReader("D:\\.Net\\OrderSplitter\\OrderSplitter\\orders.csv"))
+            using (var csv = new CsvReader(reader, config))
+
+            {
+                ords.OrderList = csv.GetRecords<Dto>().ToList<Dto>();
+            }
+
+            ords.UniqueOrderList = ords.OrderList.Select(x => x.OrderNumber).Distinct().ToList<int>();
+
+            using (var writer = new StreamWriter("D:\\.Net\\OrderSplitter\\OrderSplitter\\temp.csv"))
+            using (var csvWriter = new CsvWriter(writer, CultureInfo.InvariantCulture))
+            {
+                csvWriter.WriteHeader<Dto>();
+                csvWriter.NextRecord();
+                foreach (var record in ords.OrderList)
+                {
+                    if (record.OrderNumber == ords.UniqueOrderList.First())
+                    {
+                        csvWriter.WriteRecord(record);
+                        csvWriter.NextRecord();
+                    }
+                }
+            }
+                Console.WriteLine("\nEnd");
+
+            
         }
     }
+    
 }
+
